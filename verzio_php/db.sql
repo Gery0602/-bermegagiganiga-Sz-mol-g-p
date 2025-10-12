2a049d4f5d085579687aee1b05deed4e9e66dc04
Rezvadasz <botosb361@gmail.com>
2025-10-12 17:26:38 +0200

    .

--- verzio_php/db.sql
+++ verzio_php/db.sql
@@ -8,8 +8,10 @@ CREATE TABLE formulas ( id INT AUTO_INCREMENT PRIMARY KEY, formula_name VARCHAR(
 
 INSERT INTO roles (role_name) VALUES ('admin'), ('moderator'), ('user'), ('guest');
 
-INSERT INTO users (username, password, email, role_id) VALUES ('admin', 'admin123', 'admin@gmail.com', 1), ('moderator', 'mod123', 'mod@gmail.com', 2);
+INSERT INTO users (username, password, email, role_id) VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com', 1); --,pass: admin
 
-INSERT INTO users (username, password, email, role_id) VALUES ('uj_felhasznalo', 'jelszo', 'user@gmail.com', 3);
+INSERT INTO users (username, password, email, role_id) VALUES ('moderator', 'caacd35a900b68a8c4347d5c8564cc3d', 'mod@gmail.com', 2); --,pass: modi
+
+INSERT INTO users (username, password, email, role_id) VALUES ('felhasználó', '0662ff1d7bd7f55e1846c1378aa8d27d', 'felhasz@gmail.com', 3); --,pass: felhasz
 
 INSERT INTO formulas (formula_name, formula) VALUES ('Összeadás', 'a + b');
